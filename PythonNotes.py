5c7a0d9800c904e553747804b9355251728490f1
dhananjay <pratapsinghdhananjay7@gmail.com>
2025-08-17 15:10:45 +0530

    DSA6

---
+++ PythonNotes.py
@@ -0,0 +1,23 @@
+# Notes from Basics
+# Python Basics
+String Rules in Python:
+# 1. Strings are immutable.(Example: you cannot change a character in a string directly)
+# 2. Strings can be concatenated using the + operator.
+# 3. Strings can be repeated using the * operator.
+# 4. Strings can be indexed and sliced.
+# 5. Strings can be formatted using f-strings, format() method, or % operator.
+# 6. Strings can be iterated over using loops.
+# Indexing in Python:
+# 0-based indexing is used in Python, meaning the first character is at index 0
+# 7. Strings can be checked for membership using the in operator.
+# 8. Strings can be converted to uppercase or lowercase using upper() and lower() methods.
+# 9. Strings can be stripped of whitespace using strip() method.(# Example: '  Hello  '.strip() results in 'Hello')
+# 10. Strings can be split into a list using split() method. (Example: 'Hello World'.split() results in ['Hello', 'World'])
+# 11. Strings can be joined using join() method. (Example: ' '.join(['Hello', 'World']) results in 'Hello World')
+# 12. Strings can be checked for palindromes by comparing the string to its reverse. (Example: 'racecar' == 'racecar'[::-1]
+# 13. Strings can be checked for anagrams by sorting the characters and comparing. (Example: sorted('listen') == sorted('silent')
+# 14. Strings can be checked for substrings using the in operator. (Example: 'Hello' in 'Hello World' returns True
+# 15. Strings can be checked for prefixes and suffixes using startswith() and endswith() methods.
+
+
+    
